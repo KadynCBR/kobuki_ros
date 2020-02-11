a11cfbd8abe3263bcad5788889bbd27c54d21b3c
kadyncbr <kadyn_martinez@hotmail.com>
2020-02-10 17:05:28 -0800

    parameters add attempt.

--- kobuki_keyop/launch/keyop.launch.py
+++ kobuki_keyop/launch/keyop.launch.py
@@ -2,13 +2,11 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-
+# Parameters
 linear_vel_step = 0.05
 linear_vel_max = 1.5
 angular_vel_step = 0.33
 angular_vel_max = 6.6
-wait_for_connection_ = True
-
 
 def generate_launch_description():
     return LaunchDescription([
@@ -18,7 +16,11 @@ def generate_launch_description():
             node_name='kobuki_keyop',
             remappings=[
                 ('keyop/motor_power', 'mobile_base/commands/motor_power'),
-                ('keyop/cmd_vel', 'mobile_base/commands/velocity')
-            ]
+                ('keyop/cmd_vel', 'mobile_base/commands/velocity')],
+            parameters=[
+                ('linear_vel_step', linear_vel_step),
+                ('linear_vel_max', linear_vel_max),
+                ('angular_vel_step', angular_vel_step),
+                ('angular_vel_max', angular_vel_max)]
         ),
     ])
