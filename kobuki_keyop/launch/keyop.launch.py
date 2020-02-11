4caba446403d1ff4041d36c2da2578a00d7fffae
KadynCBR <kadyn_martinez@hotmail.com>
2020-02-10 23:49:54 -0800

    regular keyop launch working seemingly.

--- kobuki_keyop/launch/keyop.launch.py
+++ kobuki_keyop/launch/keyop.launch.py
@@ -12,15 +12,16 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='kobuki_keyop',
-            node_executable='keyop_node',
+            node_executable='kobuki_keyop_node',
             node_name='kobuki_keyop',
             remappings=[
                 ('keyop/motor_power', 'mobile_base/commands/motor_power'),
                 ('keyop/cmd_vel', 'mobile_base/commands/velocity')],
-            parameters=[
-                ('linear_vel_step', linear_vel_step),
-                ('linear_vel_max', linear_vel_max),
-                ('angular_vel_step', angular_vel_step),
-                ('angular_vel_max', angular_vel_max)]
+            # parameters=[
+            #     ('linear_vel_step', linear_vel_step),
+            #     ('linear_vel_max', linear_vel_max),
+            #     ('angular_vel_step', angular_vel_step),
+            #     ('angular_vel_max', angular_vel_max)]
+            output='screen',
         ),
     ])
